f155215702ae75cf08e6baf1cf06f1d2708ac3e0
khunlinnaing <khunlinnaingoo90@gmail.com>
2024-07-06 21:09:24 +0630

    testing

---
+++ index.php
@@ -0,0 +1,24 @@
+user_table
+
+id
+name require
+email require,email-format
+phone require,phone_format
+mobile phone_format
+birthday 18 < age
+profile
+address require
+password require, at least 8 characters
+role_id
+status
+create_date
+
+role table
+id
+value
+name
+
+login
+email_format, require
+password_length, require
+
